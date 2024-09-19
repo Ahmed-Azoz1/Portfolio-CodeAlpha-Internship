6d2dace9f62857b0a33c186215ab05c9a137257d
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-09-19 10:21:40 +0300

    Add Typed.js animation for hero text with loop effect

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ let typed1 = new Typed('.text',{
     cursorChar:'',
     loop:true,
 });
+
+let typed2 = new Typed('.hero_text',{
+    strings:['Front-End Web Developer % }','Back-End Web Developer % }','Full-Stack Web Developer % }','Freelancer % }'],
+    typeSpeed:100,
+    backSpeed:100,
+    cursorChar:'',
+    loop:true,
+});
