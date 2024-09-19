17053d359469a3af202e273e50af5c3cb1944d8c
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-09-19 11:19:14 +0300

    Configure Swiper for Testimonial Slider: Set up Swiper instance for the testimonial slider with responsive breakpoints for different screen sizes, enabling autoplay and looping functionality.

--- main.js
+++ main.js
@@ -34,3 +34,24 @@ let swiper = new Swiper('.serviceSwiper',{
         },
     },
 });
+
+let swiper2 = new Swiper('.testimonialSwiper',{
+    slidesPerView:1,
+    spaceBetween:30,
+    loop:true,
+    autoplay:true,
+    breakpoints:{
+        1200:{
+            slidesPerView:2,
+            spaceBetween:30,
+        },
+        900:{
+            slidesPerView:2,
+            spaceBetween:30,
+        },
+        500:{
+            slidesPerView:1,
+            spaceBetween:30,
+        },
+    },
+});
